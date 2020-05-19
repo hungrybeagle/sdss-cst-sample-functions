cb7a72c025a6bb6fd42a927ca6f81a0dbc1c3649
Chris Yang <cyang@deltalearns.ca>
2020-05-19 14:30:23 -0700

    Update assignment_test.py

--- assignment_test.py
+++ assignment_test.py
@@ -4,3 +4,9 @@ import assignment;
 def test_assignment():
     assert assignment.sum(3,8) == 11
     
+def test_assignment2():
+    assert assignment.sum(10,-2.5) == 7.5
+
+def test_assignment3():
+    assert assignment.sum(1,3) == 5
+    
